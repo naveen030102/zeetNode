d8ea51aa13526b6537cb4c6badf12fc0665ceb3a
naveen030102 <naveenmuthusamymuthur@gmail.com>
2022-06-24 10:51:51 +0530

    thirdcommit

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ console.log("server up and running on port ",port)
 app.get("/",(req,res)=>
 {
     console.log("get / executed")
+    res.send({name:"naveen",age:"20"})
     res.end()
 })
+app.get("/work",(req,res)=>
+{
+    console.log("working")
+    res.send({status:"running",code:"200"})
+
+    res.end()
+})
+
+
